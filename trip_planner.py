1250589e385618de121427866dad16515b4acf2f
Pd1589 <84357548+Pd1589@users.noreply.github.com>
2021-06-23 18:18:01 -0400

    Update trip_planner.py
    Added in user inputs

--- trip_planner.py
+++ trip_planner.py
@@ -1,11 +1,18 @@
 import requests
 import json
 
+
+origin_input_value = input("Please Input Departing City:")
+destination_input_value = input("Please Input Destination City:")
+year_input_value = input("Please Input Departure Year (yyyy):")
+month_input_value = input("Please Input Departure Month (mm):")
+day_input_value= input("Please Input Departure Day (dd):")
 country_input = "US"
 currency_input = "USD"
-origin_input = "JFK"
-destination_input = "SFO"
-date_input = "2021-06-24"
+origin_input = origin_input_value
+destination_input = destination_input_value
+date_input = year_input_value+"-"+month_input_value+"-"+day_input_value
+
 
 url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/{country}/{currency}/en-US/{origin}-sky/{destination}-sky/{date}".format(country=country_input, currency=currency_input, origin=origin_input,destination=destination_input,date=date_input)
 
@@ -18,6 +25,9 @@ headers = {
 
 response = requests.request("GET", url, headers=headers, params=querystring)
 
+print("-------------------------------------------------------")
+print("                                                       ")
+print("Cheapest Flight Available:")
 print(response.text)
 #print(response.text["Quotes"])
 
